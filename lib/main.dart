d0088d73749d1c1cbdc56bfad7b19658b02d9bd4
2478643035 <2478643035@qq.com>
2026-05-12 01:31:43 +0800

    Integrate SenseVoice offline STT

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:sherpa_onnx/sherpa_onnx.dart' as sherpa;
 
 import 'app.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  if (Platform.isAndroid || Platform.isIOS) {
+    sherpa.initBindings();
+  }
 
   runApp(const ProviderScope(child: DaylineApp()));
 }
